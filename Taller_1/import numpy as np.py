9379a4a9b3cb0aac5b26eb4d6c41f100192aa78c
David Monroy <david_monroyhernandez@hotmail.com>
2023-08-28 21:34:34 -0500

    1.3

--- Taller_1/import numpy as np.py
+++ Taller_1/import numpy as np.py
@@ -7,8 +7,12 @@ def tuplas(archivo):
     prim_T = arch.readline()
     lam = ""
     n = ""
-    while prim_T != "":
-        if prim_T == "    data: |\n":
+    while prim_T != "    data: |\n":
+        prim_T = arch.readline()
+    
+    if prim_T == "    data: |\n":
+        prim_T = arch.readline()
+        while prim_T != "" and prim_T != "SPECS:\n":
             for i in prim_T:
                 if i != " ":
                     if len(lam) <= 4:
@@ -16,12 +20,11 @@ def tuplas(archivo):
                     elif len(n) <= 8:
                         n += str(i)
             print ((float(lam),float(n)))
-        lam=""
-        n=""
-    else:
-        prim_T=arch.readline()
+            prim_T = arch.readline( )
+            lam=""
+            n=""
 
     return
 
-ruta="c:\\Users\\ekkol\\OneDrive\\Documentos\\David Monroy\\FISI2526-MetCompCompl-202320-Ortiz-Monroy\\Taller_1\\Adhesivos Ópticos\\Iezzi.yml"
+ruta='c:\\Users\\juanm\\OneDrive\\Documentos\\Felipe universidad\\FISI2526-MetCompCompl-202320-Ortiz-Monroy\\Taller_1\\Adhesivos Ópticos\\Iezzi.yml'
 tuplas(ruta)
