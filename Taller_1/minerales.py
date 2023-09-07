e2467cfe155b0fb7b699567e700f49ac5f96d71c
Andres Esteban Ortiz Jimenez <andresestebanortizjimenez@MacBook-Air-de-Andres-2.local>
2023-09-07 09:46:53 -0500

    minerales.py

---
+++ Taller_1/minerales.py
@@ -0,0 +1,43 @@
+import matplotlib as mpl
+
+class Mineral:
+    
+    def __init__(self, nombre, dureza, lustre, rompimiento_por_fractura, color, composicion, sistema_cristalino, specific_gravitiy):
+        
+        self.nombre=nombre
+        self.dureza=dureza
+        self.lustre=lustre
+        self.rompimiento_por_fractura=rompimiento_por_fractura
+        self.color=color
+        self.composicion=composicion
+        self.sistema_cristalino=sistema_cristalino
+        self.specific_gravity=specific_gravitiy
+        
+    def silicato (self):
+        a=True
+        if "Si" and "O" in self.composicion:
+            return a
+        else:
+            a=False
+            return a
+        
+    def densidad (self):
+        densidad= 997*self.specific_gravity
+        return densidad
+            
+    def color_comun(self):
+        return( mpl.colors(self.color))    
+        
+    def dureza_rompimiento_organizacion (self):
+        tipo_de_rompimiento=""
+        if self.rompimiento_por_fractura == False:
+            tipo_de_rompimiento= "No se rompe por fractura"
+        else:
+            tipo_de_rompimiento="Se rompe por fractura"
+
+        return(str(self.dureza)+", "+tipo_de_rompimientols+", "+(self.sistema_cristalino))
+        
+
+mineral1= Mineral(nombre="grafito", dureza=1.5, lustre="METÁLICO", rompimiento_por_fractura=False, color="#5f6168", composicion="SiO", sistema_cristalino="HEXAGONAL", specific_gravitiy=2.2)
+
+print(mineral1.dureza_rompimiento_organizacion())
