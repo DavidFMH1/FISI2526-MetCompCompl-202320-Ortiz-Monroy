44a506eadcd6827bdcfb300c9931d3bda25c3e01
DavidFMH1 <david_monroyhernandez@hotmail.com>
2023-09-07 23:47:49 -0500

    9

--- Taller_1/grafica.py
+++ Taller_1/grafica.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-archivo = pd.read_csv('C:\\Users\\david\\OneDrive\\Documentos\\programas\\Metodos\\FISI2526-MetCompCompl-202320-Ortiz-Monroy\\Taller_1\\graphite_mceligot_2016.csv', sep='\t')
+archivo_g = pd.read_csv('C:\\Users\\david\\OneDrive\\Documentos\\programas\\Metodos\\FISI2526-MetCompCompl-202320-Ortiz-Monroy\\Taller_1\\graphite_mceligot_2016.csv', sep='\t')
+archivo_o = pd.read_csv('C:\\Users\\david\\OneDrive\\Documentos\\programas\\Metodos\\FISI2526-MetCompCompl-202320-Ortiz-Monroy\\Taller_1\\olivine_angel_2017.csv', sep='\t')
 
-d_a = np.array(archivo)
+d_a = np.array(archivo_g)
+d_b = np.array(archivo_o)
 
 def leer_array(a):
     m = []
@@ -30,10 +32,16 @@ def grafica(l):
         alpha = (dy/dx)*(1/y[i])
         
         plt.scatter(y[i],alpha)
-    
+    plt.ylabel("Alpha")
+    plt.xlabel("Temperatura")
+    plt.savefig('C:\\Users\\david\\OneDrive\\Documentos\\programas\\Metodos\\FISI2526-MetCompCompl-202320-Ortiz-Monroy\\Taller_1\\olivine_angel_2017')
     plt.scatter(x,y)
-    plt.show()
+    plt.ylabel("Volumen")
+    plt.xlabel("Temperatura")
+    plt.savefig('C:\\Users\\david\\OneDrive\\Documentos\\programas\\Metodos\\FISI2526-MetCompCompl-202320-Ortiz-Monroy\\Taller_1\\olivine_angel_2017')
     
 l = leer_array(d_a)
+k = leer_array(d_b)
 
 grafica(l)
+#grafica(k)
