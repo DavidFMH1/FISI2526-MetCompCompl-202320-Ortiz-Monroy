2d4b05fe7042e5f4a40e482fa6fedf48fcaad621
DavidFMH1 <david_monroyhernandez@hotmail.com>
2023-09-23 16:19:11 -0500

    punto 3

--- Taller_2/Punto_3.py
+++ Taller_2/Punto_3.py
@@ -1,6 +1,9 @@
+#Punto 3.1
+
 import numpy as np
 import sympy as sym
 import matplotlib.pyplot as plt
+import math
 
 _x = sym.symbols('x')
 
@@ -81,20 +84,80 @@ def GetAllRootsGLag(n):
     
     return Roots
 
-def GetWeigthsLag(n):
+def GetWeigthsLag(n,x):
     
     weigths = np.array([])
     roots = GetAllRootsGLag(n)
-    L_n1 = sym.lambdify([_x],GetLaguerre(n+1,_x),'numpy')
+    L_n1 = sym.lambdify([x],GetLaguerre(n+1,x),'numpy')
     
     for i in roots:
+        
         w = i/(((n+1)**2)*(L_n1(i))**2)
+        
         if w not in weigths:
+            
             weigths = np.append(weigths,w)
             
     return weigths
     
-
+print('polinomios de Laguerre')
 print(GetLaguerre(2,_x))
 print(GetAllRootsGLag(2))
-print(GetWeigthsLag(2))
+print(GetWeigthsLag(2,_x))
+
+#Punto 3.2
+
+def GetHermite(n,x):
+    if n == 0:
+        poly = sym.Number(1)
+    elif n == 1:
+        poly = 2*x
+    else:
+        poly = 2*x*GetHermite(n-1,x) - sym.diff(GetHermite(n-1,x),x,1)
+    return sym.expand(poly,x)
+
+def DiffHermite(n,x):
+    pn = GetHermite(n,x)
+    return sym.diff(pn,x,1)
+
+def GetAllRootsGHer(n):
+    
+    c = np.sqrt(4*n+1)
+    xn = np.linspace(-c,c,100)
+    
+    Hermite = np.array([])
+    DHermite = np.array([])
+    
+    for i in range(n+1):
+        Hermite =  np.append(Hermite,GetHermite(i,_x))
+        DHermite = np.append(DHermite,DiffHermite(i,_x))
+    
+    poly = sym.lambdify([_x],Hermite[n],'numpy')
+    Dpoly = sym.lambdify([_x],DHermite[n],'numpy')
+    Roots = GetRoots(poly,Dpoly,xn)
+
+    if len(Roots) != n:
+        ValueError('El número de raíces debe ser igual al n del polinomio.')
+    
+    return Roots
+
+def GetWeigthsHer(n,x):
+    
+    roots = GetAllRootsGHer(n)
+    l_h1 = sym.lambdify([x],GetHermite(n-1,x),'numpy')
+    weigths = np.array([])
+    
+    for i in roots:
+        
+        w = (2**(n-1)*math.factorial(n)*np.sqrt(n))/((n**2)*(l_h1(i)**2))
+        
+        if w not in weigths:
+            
+            weigths = np.append(weigths,w)
+    
+    return weigths
+
+print('polinomios de Hermite')
+print(GetHermite(2,_x))
+print(GetAllRootsGHer(2))
+print(GetWeigthsHer(2,_x))
