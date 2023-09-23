576c2f59a05b33d1d044cdea28a4f378cf88a97e
DavidFMH1 <david_monroyhernandez@hotmail.com>
2023-09-23 15:41:18 -0500

    punto 3

--- Taller_2/Punto_3.py
+++ Taller_2/Punto_3.py
@@ -1,23 +1,21 @@
-#laguerre
-
 import numpy as np
 import sympy as sym
 import matplotlib.pyplot as plt
 
-x = sym.symbols('x')
+_x = sym.symbols('x')
 
-def laguerre(n,x):
+def GetLaguerre(n,x):
     if n==0:
         poly = sym.Number(1)
     elif n==1:
         poly = - x + 1
     else:
-        poly = ((2*(n-1)+1-x)*laguerre(n-1,x)-((n-1)*laguerre(n-2,x)))/(n)
+        poly = ((2*(n-1)+1-x)*GetLaguerre(n-1,x)-((n-1)*GetLaguerre(n-2,x)))/(n)
    
     return sym.expand(poly,x)
 
-def dflaguerre(n,x):
-    pn = laguerre(n,x)
+def DiffLaguerre(n,x):
+    pn = GetLaguerre(n,x)
     return sym.diff(pn,x,1)
 
 def GetNewton(f,df,xn,itmax=10000,precision=1e-14):
@@ -62,7 +60,7 @@ def GetRoots(f,df,x,tolerancia = 10):
     
     return Roots
 
-def GetAllRootsGLeg(n):
+def GetAllRootsGLag(n):
 
     j = n+((n-1)*np.sqrt(n))
     xn = np.linspace(0,j,100)
@@ -71,11 +69,11 @@ def GetAllRootsGLeg(n):
     DLegendre = np.array([])
     
     for i in range(n+1):
-        Legendre =  np.append(Legendre,laguerre(i,x))
-        DLegendre = np.append(DLegendre,dflaguerre(i,x))
+        Legendre =  np.append(Legendre,GetLaguerre(i,_x))
+        DLegendre = np.append(DLegendre,DiffLaguerre(i,_x))
     
-    poly = sym.lambdify([x],Legendre[n],'numpy')
-    Dpoly = sym.lambdify([x],DLegendre[n],'numpy')
+    poly = sym.lambdify([_x],Legendre[n],'numpy')
+    Dpoly = sym.lambdify([_x],DLegendre[n],'numpy')
     Roots = GetRoots(poly,Dpoly,xn)
 
     if len(Roots) != n:
@@ -83,5 +81,20 @@ def GetAllRootsGLeg(n):
     
     return Roots
 
-print(laguerre(2,x))
-print(GetAllRootsGLeg(2))
+def GetWeigthsLag(n):
+    
+    weigths = np.array([])
+    roots = GetAllRootsGLag(n)
+    L_n1 = sym.lambdify([_x],GetLaguerre(n+1,_x),'numpy')
+    
+    for i in roots:
+        w = i/(((n+1)**2)*(L_n1(i))**2)
+        if w not in weigths:
+            weigths = np.append(weigths,w)
+            
+    return weigths
+    
+
+print(GetLaguerre(2,_x))
+print(GetAllRootsGLag(2))
+print(GetWeigthsLag(2))
