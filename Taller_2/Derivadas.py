591622d0378835f304a003e0b15aa173cbc7c007
Andres Esteban Ortiz Jimenez <andresestebanortizjimenez@MacBook-Air-de-Andres-2.local>
2023-09-25 17:50:51 -0500

    Montecarlo e Imagen_1

--- Taller_2/Derivadas.py
+++ Taller_2/Derivadas.py
@@ -6,9 +6,10 @@ import math
 def flujo_magnetico(t,r=0.025/2,w=3.5,B=0.05,f=1000):
     return np.pi*(r**2)*B*np.sin(w*t)*np.cos(2*np.pi*f*t)
 
-def derivada(t,f):
+def derivada(t,f,b):
     h=t[1]-t[0]
-    return (f(t+h)-f(t-h))/(2*h)
+    print(f(b))
+    #return (f(t+h)-f(t-h))/(2*h)
 
 def corriente(dB,R=1750):    
     return -1/R*(dB)
@@ -21,7 +22,7 @@ def Newton(B,dB,x_n,itmax=100,precision=1e-10):
             x_n1=x_n-B(x_n)/dB(x_n,B)
         except ZeroDivisionError:
             print('División por cero')
-        n_n=x_n1
+        x_n=x_n1
         it+=1
     if it==itmax:
         return False
@@ -44,15 +45,13 @@ t=np.linspace(0,1,1000)
 B=flujo_magnetico(t)
 dB=derivada(t,flujo_magnetico)
 I=corriente(dB)
-dI=derivada(t,corriente(dB))
-
-"""
-#print(raices(t))
+dI=derivada(t,corriente(),dB)
+ 
+print(dI)
 #plt.plot(t,B,label="Flujo magnético")
 #plt.plot(t,dB,label="Derivada Flujo")
 #plt.plot(t,I,label="Corriente",color='g')
 #plt.axhline(y=0,color='r')
-plt.legend()
-plt.show()
-"""
-print(dI)
+#print(dI)
+#plt.legend()
+#plt.show()
